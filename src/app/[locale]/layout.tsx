2b4500dba6be39784ebfc4dc2fafe7626949556a
Joao Carmassi <joaovitorcarmassi@gmail.com>
2025-10-29 10:30:24 -0300

    Moves analytics scripts to root layout for consistency
    Centralizes Google Analytics and Tag Manager setup in the application root layout to ensure tracking is consistently applied across all pages, including the default locale. Simplifies per-locale layouts by removing duplicate scripts and related meta tags.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from '@/components/theme-provider';
 import MessagesProvider from '@/context/messages';
 import { Metadata } from 'next';
 import { Raleway } from 'next/font/google';
-import Script from 'next/script';
 import '../globals.css';
 import { getMessages } from '@/utils/getMessages';
 import { ReactLenis } from 'lenis/react';
@@ -63,49 +62,8 @@ const RootLayout = async ({ children, params }: Props) => {
   const links = t('links');
 
   return (
-    <html lang={locale} suppressHydrationWarning>
-      <head>
-        <meta
-          name='google-site-verification'
-          content='fROZF15LJteQ084SiUI76J86k51o7lG2n3bzHfCh69Y'
-        />
-      </head>
+    <>
       <body className={`${raleway.variable} font-main antialiased`}>
-        {/* Google Analytics */}
-        <Script
-          src='https://www.googletagmanager.com/gtag/js?id=G-ZPXTYH8YVL'
-          strategy='afterInteractive'
-        />
-        <Script id='google-analytics' strategy='afterInteractive'>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-ZPXTYH8YVL');
-          `}
-        </Script>
-
-        {/* Google Tag Manager */}
-        <Script id='google-tag-manager' strategy='afterInteractive'>
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N2GFFDFJ');
-          `}
-        </Script>
-
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src='https://www.googletagmanager.com/ns.html?id=GTM-N2GFFDFJ'
-            height='0'
-            width='0'
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-
         <ReactLenis
           options={{
             lerp: 0.1,
@@ -120,7 +78,7 @@ const RootLayout = async ({ children, params }: Props) => {
           </MessagesProvider>
         </ReactLenis>
       </body>
-    </html>
+    </>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,53 @@
+import Script from 'next/script';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <head>
+        <meta
+          name='google-site-verification'
+          content='fROZF15LJteQ084SiUI76J86k51o7lG2n3bzHfCh69Y'
+        />
+      </head>
+      {/* Google Analytics */}
+      <Script
+        src='https://www.googletagmanager.com/gtag/js?id=G-ZPXTYH8YVL'
+        strategy='afterInteractive'
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZPXTYH8YVL');
+          `}
+      </Script>
+
+      {/* Google Tag Manager */}
+      <Script id='google-tag-manager' strategy='afterInteractive'>
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N2GFFDFJ');
+          `}
+      </Script>
+
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src='https://www.googletagmanager.com/ns.html?id=GTM-N2GFFDFJ'
+          height='0'
+          width='0'
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+      {children}
+    </html>
+  );
 }
